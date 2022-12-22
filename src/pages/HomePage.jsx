da29128539cbdf63dd8e3a5b70e219dec17c9dd9
Nikolay Brovkin <bronik004@gmail.com>
2022-12-22 14:44:05 +0300

    feat: added render cards

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,46 +3,65 @@ import { useNavigate } from 'react-router-dom';
 import { List } from '../components/List';
 import { Card } from '../components/Card';
 import { Controls } from '../components/Controls';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectAllCountries,
+  selectCountriesInfo,
+} from '../store/countries/countries-selectors';
+import { useEffect } from 'react';
+import { loadCountries } from '../store/countries/countries-action';
 
 export const HomePage = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const countries = useSelector(selectAllCountries);
+  const { status, error, qty } = useSelector(selectCountriesInfo);
 
-  const countries = [];
+  useEffect(() => {
+    if (!qty) {
+      dispatch(loadCountries());
+    }
+  }, [qty, dispatch]);
 
   return (
     <>
       <Controls />
 
-      <List>
-            {countries.map((c) => {
-              const countryInfo = {
-                img: c.flags.png,
-                name: c.name,
-                info: [
-                  {
-                    title: 'Population',
-                    description: c.population.toLocaleString(),
-                  },
-                  {
-                    title: 'Region',
-                    description: c.region,
-                  },
-                  {
-                    title: 'Capital',
-                    description: c.capital,
-                  },
-                ],
-              };
+      {error && <h2>Can't fetch data</h2>}
+      {status === 'loading' && <h2>Loading...</h2>}
 
-              return (
-                <Card
-                  key={c.name}
-                  onClick={() => navigate(`/country/${c.name}`)}
-                  {...countryInfo}
-                />
-              );
-            })}
-          </List>
+      {status === 'received' && (
+        <List>
+          {countries.map((c) => {
+            const countryInfo = {
+              img: c.flags.png,
+              name: c.name,
+              info: [
+                {
+                  title: 'Population',
+                  description: c.population.toLocaleString(),
+                },
+                {
+                  title: 'Region',
+                  description: c.region,
+                },
+                {
+                  title: 'Capital',
+                  description: c.capital,
+                },
+              ],
+            };
+
+            return (
+              <Card
+                key={c.name}
+                onClick={() => navigate(`/country/${c.name}`)}
+                {...countryInfo}
+              />
+            );
+          })}
+        </List>
+      )}
     </>
   );
 };
--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,7 +1,8 @@
 import {combineReducers} from "redux";
 import {themeReducers} from "./theme/theme-reducers";
+import {countriesReducer} from "./countries/countries-reducer";
 
 export const rootReducer = combineReducers({
   theme: themeReducers,
-  countries: combineReducers,
+  countries: countriesReducer,
 });
