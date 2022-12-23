640a57c846a2133bc32e20ced946cec562815882
Nikolay Brovkin <bronik004@gmail.com>
2022-12-23 23:02:51 +0300

    ts: added region and country types

---
+++ src/types/country.ts
@@ -0,0 +1,32 @@
+import {Region} from "./region";
+
+type Currency = {
+    code: string,
+    name: string,
+    symbol: string,
+}
+
+type Language = {
+    iso693_1: string,
+    iso693_2: string,
+    name: string,
+    nativeName: string,
+}
+
+export type Country = {
+    name: string,
+    nativeName: string,
+    flag: string,
+    flags: [
+        {png: string},
+        {svg: string}
+    ],
+    region: Region,
+    subregion: string,
+    capital: string,
+    population: number,
+    topLevelDomain: string[],
+    borders: string[],
+    currencies: Currency[],
+    language: Language[],
+}
